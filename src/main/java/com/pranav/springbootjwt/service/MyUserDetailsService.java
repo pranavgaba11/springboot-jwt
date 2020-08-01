ecd85adb81659651764dff76b1ef0876ec426bad
fintechlmt <pranavgaba11@gmail.com>
2020-08-01 16:40:04 +0530

    initial commit

---
+++ src/main/java/com/pranav/springbootjwt/service/MyUserDetailsService.java
@@ -0,0 +1,18 @@
+package com.pranav.springbootjwt.service;
+
+import java.util.ArrayList;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+	@Override
+	public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+		return new User("pranav", "pranav", new ArrayList<>());
+	}
+}
---
+++ src/test/java/com/pranav/springbootjwt/SpringbootJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranav.springbootjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
